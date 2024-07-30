e75a5ddaab6c076e79e212797032760accff092e
Jose Luis Doria <joseluisdoria7@gmail.com>
2024-07-29 23:36:01 -0500

    Update stored_procedures.sql

--- sql_project/database_objects/stored_procedures.sql
+++ sql_project/database_objects/stored_procedures.sql
@@ -1,1 +1,12 @@
 USE empresa_ferretera;
+
+-- Crear el Porcedimiento Alamacenado sp_ventas_producto
+DELIMITER //
+CREATE PROCEDURE `sp_ventas_producto`( IN producto_id INT)
+BEGIN
+    -- Recuperar todos los detalles de `detalle_pedidos` para el `producto_id` dado
+    SELECT *
+    FROM detalle_pedidos
+    WHERE id_producto = producto_id;
+END //
+DELIMITER ;
