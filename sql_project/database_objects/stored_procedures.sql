8d3f566c6ddc3cd5013c531e506b9b28a5d1bf11
Jose Luis Doria <joseluisdoria7@gmail.com>
2024-07-29 23:57:51 -0500

    Update stored_procedures.sql

--- sql_project/database_objects/stored_procedures.sql
+++ sql_project/database_objects/stored_procedures.sql
@@ -1,6 +1,10 @@
 USE empresa_ferretera;
 
--- Crear el Porcedimiento Alamacenado sp_ventas_producto
+Drop procedure sp_ventas_producto;
+Drop procedure sp_agregar_cliente;
+
+    
+-- Crear el Procedimiento Almacenado sp_ventas_producto
 DELIMITER //
 CREATE PROCEDURE `sp_ventas_producto`( IN producto_id INT)
 BEGIN
@@ -9,4 +13,19 @@ BEGIN
     FROM detalle_pedidos
     WHERE id_producto = producto_id;
 END //
+DELIMITER ;
+
+-- Crear el Procedimiento Almacenado sp_agregar_cliente
+DELIMITER //
+-- Sp para agregar un Cliente Nuevo
+CREATE PROCEDURE sp_agregar_cliente(
+    IN p_nombre VARCHAR(100),
+    IN p_telefono VARCHAR(20),
+    IN p_email VARCHAR(100)
+)
+BEGIN
+    INSERT INTO clientes (nombre, telefono, email)
+    VALUES (p_nombre, p_telefono, p_email);
+END //
+
 DELIMITER ;
