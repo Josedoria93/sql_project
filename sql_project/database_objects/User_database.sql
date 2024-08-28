0c7caf82b809ea32be625675f1d782d5477ea153
Jose Luis Doria <joseluisdoria7@gmail.com>
2024-08-28 03:31:10 +0000

    Update

---
+++ sql_project/database_objects/User_database.sql
@@ -0,0 +1,38 @@
+USE empresa_ferretera;
+
+-- Crearemos 1 rol que tiene todos los permisos
+
+DROP ROLE IF EXISTS 'DBA';
+CREATE ROLE 'DBA';
+
+-- Se Otorgan los permisos
+
+-- ROL DBA
+-- Todos los permisos
+GRANT ALL PRIVILEGES ON repositor_ruedas.* TO 'DBA';
+
+-- Creación de usuarios
+	
+-- DBA
+DROP USER IF EXISTS
+	'josedp'@'%',
+	
+
+CREATE USER 'josedp'@'%' IDENTIFIED BY '1594'
+	FAILED_LOGIN_ATTEMPTS 4
+	PASSWORD_LOCK_TIME 3
+	PASSWORD EXPIRE INTERVAL 200 DAY;
+
+-- Se Otorgan los roles
+
+GRANT 'DBA' TO
+	'josedp'@'%';
+
+-- Activación de roles por defecto
+
+SET DEFAULT ROLE 'DBA'
+	TO 'josedp'@'%';
+
+-- Actualización de privilegios
+
+FLUSH PRIVILEGES;
